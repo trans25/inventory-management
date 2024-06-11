e48b338f2318ee429a296d39a3f62426037f8329
elias mashia <mashiaes@gmail.com>
2024-06-11 11:28:41 +0200

    add nav links

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,23 +1,50 @@
 import React from 'react';
-import styles from './Header.scss'
-import {Link} from 'react-router-dom'
+import styles from './header.css'
+import {Link, NavLink} from 'react-router-dom'
 
-const logo =(<div className={styles.logo}>
+const logo =(<div className="logo">
     <Link to="/">
-    <h2>
-        Shop<span>Ecomm</span>
+    <h2 className='h2'>
+        Shop<span className='span'>Ecomm</span>
     </h2>
     </Link>
 
 </div>
 )
-
+const activeLink = ({isActive}) => (
+    isActive ? `${styles.active}` : ""
+  );
+  
 const Header = () => {
   return (
     <header >
-      Header
-      <div className={styles.header}>
+      
+      <div className="header">
        {logo}
+       <nav>
+        <ul>
+<li>
+    <NavLink to="/shop" className={activeLink}>Shop</NavLink>
+
+</li>
+        </ul>
+        <div className={styles['header-right']}>
+
+            <span className={styles.links}>
+                <NavLink to={'login'} className={activeLink}>
+                Login</NavLink>
+
+                <NavLink to={'register'} className={activeLink}>
+                Register</NavLink>
+
+                <NavLink to={'order-history'} className={activeLink}>
+                My Orders</NavLink>
+
+
+            </span>
+
+        </div>
+       </nav>
       </div>
     </header>
   );
